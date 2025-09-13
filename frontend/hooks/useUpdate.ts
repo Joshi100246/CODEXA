634da657ada8a9d829911519162a2bdd3d01d2d5
Joshi100246 <159691354+Joshi100246@users.noreply.github.com>
2025-09-13 18:41:01 +0530

    Update useUpdate.ts

--- frontend/hooks/useUpdate.ts
+++ frontend/hooks/useUpdate.ts
@@ -1,1 +1,14 @@
+import { useEffect, useRef } from 'react';
+
+export default function useUpdate(fn: () => void) {
+  const mounted = useRef(false);
+
+  useEffect(() => {
+    if (mounted.current) {
+      fn();
+    } else {
+      mounted.current = true;
+    }
+  });
+}
 
