6735087852c0bee0275fb136fda20b060381b307
Joshi100246 <159691354+Joshi100246@users.noreply.github.com>
2025-09-13 19:51:26 +0530

    Update usePrevious.tsx

--- frontend/hooks/usePrevious.tsx
+++ frontend/hooks/usePrevious.tsx
@@ -1,4 +1,10 @@
 // frontend/hooks/usePrevious.ts
-export default function usePrevious<T>(value: T) {
-  return null;
+import { useRef, useEffect } from 'react';
+
+export function usePrevious<T>(value: T) {
+  const ref = useRef<T>();
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  return ref.current;
 }
