c0d8f665f7dfb6b24d359eac3a28aed60c8faa6c
Joshi100246 <159691354+Joshi100246@users.noreply.github.com>
2025-09-13 19:49:38 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,  // needed if you use the /app folder (Next 13+)
-  },
+  // Remove experimental.appDir
 };
 
 module.exports = nextConfig;
